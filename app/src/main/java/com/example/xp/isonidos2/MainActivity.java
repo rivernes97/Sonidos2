0c5291b45a8d1698b0807b1d1203ab2f4b9253a8
ernes <ernesdevicente@hotmail.com>
2018-04-17 12:38:36 +0200

    terminado

--- app/src/main/java/com/example/xp/isonidos2/MainActivity.java
+++ app/src/main/java/com/example/xp/isonidos2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.xp.isonidos2;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -49,22 +51,45 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void sonido(View view){
-        Log.i("etiqueta: ", findViewById(view.getId()).getTag().toString());
-        Button b = (Button) findViewById(view.getId());
-        MediaPlayer m = new MediaPlayer();
-        m = MediaPlayer.create(this, (int)findViewById(view.getId()).getTag());
-        m.start();
-        m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                mediaPlayer.stop();
-                if (mediaPlayer != null) {
-                    mediaPlayer.release();
-                }
 
-            }
-        });
+    public void sonido(View view){
+        //Log.i("etiqueta: ", findViewById(view.getId()).getTag().toString());
+        Button b = (Button) findViewById(view.getId());
+        String nombre = b.getText().toString();
+        if (nombre.substring(0,2).contains("v_")) {
+            VideoView videoview = (VideoView) findViewById(R.id.videoView);
+            Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+view.getTag());
+            videoview.setVideoURI(uri);
+            videoview.start();
+        } else {
+            MediaPlayer m = new MediaPlayer();
+            m = MediaPlayer.create(this, (int) findViewById(view.getId()).getTag());
+            m.start();
+            m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.stop();
+                    if (mediaPlayer != null) {
+                        mediaPlayer.release();
+                    }
+                }
+            });
+        }
+    }
+    public void sonidoCopiar(View view){
+        Button b = (Button) findViewById(view.getId());
+        String nombre = b.getText().toString();
+
+
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("audio/*");
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/raw/"+nombre+".mp3");
+        Log.i("nombre: ", uri.toString());
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
+
     }
 
     private LinearLayout creaLineaBotones(int numeroLinea){
@@ -77,8 +102,8 @@ public class MainActivity extends AppCompatActivity {
         linea.setLayoutParams(parametros);
         linea.setId(numeroLinea);
         return linea;
-    }
 
+    }
     private Button creaBoton(int i, Field[] _listaCanciones){
         LinearLayout.LayoutParams parametrosBotones = new LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.MATCH_PARENT);
         parametrosBotones.weight = 1;
@@ -88,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         b.setLayoutParams(parametrosBotones);
         b.setText(_listaCanciones[i].getName());
         b.setTextColor(Color.WHITE);
-        b.setBackgroundColor(Color.BLUE);
+        b.setBackgroundColor(Color.RED);
         b.setAllCaps(false); //todas las letras del botón en minúscula
         int id = this.getResources().getIdentifier(_listaCanciones[i].getName(), "raw", this.getPackageName());
         b.setTag(id);
@@ -98,6 +123,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sonido(view);
+            }
+        });
+
+        b.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                sonidoCopiar(v);
+                return true;
             }
         });
         return b;
